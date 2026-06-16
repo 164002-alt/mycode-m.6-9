e1199546e5869fb096adf0716946ce6567f858b6
164002-alt <164002@wsk.ac.th>
2026-06-16 02:57:36 +0000

    program03.py

---
+++ program03.py
@@ -0,0 +1,7 @@
+width = float(input("กรุณาใส่ความกว้าง"))#รับค่าความยาวกับความกว้าง
+length = float(input("กรุณาใส่ความยาว"))
+
+area = width * length #คำนวณพื้นที่
+
+print("พื้นที่รูปสี่เหลี่ยมมุมฉาก =",area) #แสดงผลลัพธื
+print("author: Chaichanok Sumcharoensuk m.6/9")
